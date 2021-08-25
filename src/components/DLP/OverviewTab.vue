629e9bf58d839ff4467d6f61aa3bc5c6a5eb5ad3
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2021-08-25 17:55:47 -0400

    add contributors to overview tab

--- src/components/DLP/OverviewTab.vue
+++ src/components/DLP/OverviewTab.vue
@@ -1,11 +1,37 @@
 <template>
   <div>
-    <!-- TODO: implement this -->
+    <v-card>
+      <v-card-title>
+        <v-icon>mdi-account-multiple</v-icon>
+        <span class="ml-2">Contributors</span>
+      </v-card-title>
+      <v-card-text>
+        <v-chip
+          v-for="(contributor, i) in contributors"
+          :key="i"
+          style="margin: 5px;"
+        >
+          {{ contributor.name }}
+          <a
+            v-if="contributor.identifier"
+            :href="`https://orcid.org/${contributor.identifier}`"
+            target="_blank"
+            class="ml-1"
+          >
+            <img
+              alt="ORCID logo"
+              src="https://info.orcid.org/wp-content/uploads/2019/11/orcid_16x16.png"
+              width="16"
+              height="16"
+            ></a>
+        </v-chip>
+      </v-card-text>
+    </v-card>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@vue/composition-api';
+import { computed, defineComponent } from '@vue/composition-api';
 
 export default defineComponent({
   name: 'OverviewTab',
@@ -22,7 +48,16 @@ export default defineComponent({
     },
   },
   setup(props, ctx) {
+    const { meta } = props;
+
+    const contributors = computed(
+      () => meta.contributor.filter(
+        (author: any) => author.schemaKey === 'Person' && author.includeInCitation,
+      ),
+    );
+
     return {
+      contributors,
     };
   },
 });
